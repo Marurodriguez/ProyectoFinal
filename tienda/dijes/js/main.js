e4291a1006c54f594209d70e30052a74accd7416
Marurodriguez <rodriguezsmarisol@gmail.com>
2022-05-12 22:07:29 -0300

    Update main.js

--- tienda/dijes/js/main.js
+++ tienda/dijes/js/main.js
@@ -1,98 +1,126 @@
-
-
-const catalogoaros = ["ARO1", "ARO2" , "ARO3"];
-const catalogodijes = ["DIJE1", "DIJE2"];
-const catalogo = [catalogoaros, catalogodijes];
-
-class productos {
-    constructor(nombre, color, precio, stock, disponible){
+class producto {
+    constructor(id, nombre, precio, stock, disponible){
+        this.id = id;
         this.nombre = nombre;
-        this.color = color;
         this.precio = precio;
         this.stock = stock;
         this.disponible = disponible;
     }
 }
 
-const producto1 = new productos ("ARO1", "Naranja", 2500, 5, true);
-const producto2 = new productos ("ARO2", "Violeta", 3000, 5, true);
-const producto3 = new productos ("ARO3", "Azul", 2000, 5, false);
-const producto4 = new productos ("DIJE1", "Azul", 2000, 5, true);
-const producto5 = new productos ("DIJE2", "Rosa", 2000, 5, true);
-console.log(catalogo);
+const prod1 = new producto (1,"ARO1", 2500, 5, true);
+const prod2 = new producto (2, "ARO2", 3000, 5, true);
+const prod3 = new producto (3, "ARO3", 2000, 5, false);
+const prod4 = new producto (1, "DIJE1", 2000, 5, true);
+const prod5 = new producto (2, "DIJE2", 2000, 5, true);
 
-catalogodijes.push(new productos("DIJE3", "NEGRO", "2500", 5, true));
-console.log(catalogo);
+const catalogoaros = [prod1, prod2, prod3];
+const catalogodijes = [prod4, prod5];
 
+console.log(catalogoaros);
+console.log(catalogodijes);
 
+const prod6 = catalogodijes.push(new producto(3, "DIJE3", "2500", 5, true));
+console.log(catalogodijes);
+
+console.log(catalogodijes[2])
 
 let color
 let unidades
-let dijeazul
-let dijerosa
-let dijenegro
+let prodElegido
+let dije1
+let dije2
+let dije3
 
-function elegir (){
-    color = prompt("Ingrese el color de dije: AZUL / ROSA / NEGRO");
+const elegirColor = () => { 
+    color = prompt("Ingrese el color de dije: NARANJA / VIOLETA / AZUL");
 
-    if (color === "AZUL"|| color === "Azul"|| color === "azul") {
-        dijeazul = true;
-        unidades = parseInt(prompt("Ok, cuantas unidades?"))
+    while (color === ""){
+        color = prompt("Ingrese el color de aro: NARANJA / VIOLETA / AZUL");  
     }
-    else if (color === "ROSA"||color === "Rosa"|| color === "rosa"){
+    
+    if (color === "NARANJA"|| color === "Naranja"|| color === "naranja"|| color === "VIOLETA"||color === "Violeta"|| color === "violeta"|| color === "AZUL"|| color === "Azul" || color === "azul"){ 
         unidades = parseInt(prompt("Ok, cuantas unidades?"))
-        dijerosa = true;}
-    else if (color === "NEGRO"|| color === "Negro" || color === "negro"){
-        unidades = parseInt(prompt("Ok, cuantas unidades?"))
-        dijenegro = true;}
+
+    }
     else {
         alert("No entiendo")
+        elegirColor();  
+    }  
+    }
+
+    
+
+    const vender =()=>{
+
+        if (dije1 === true){
+                if (prod4.disponible === true && prod4.stock >= unidades){
+                    alert("Debes abonar $" + unidades * prod4.precio)
+                } 
+                else { 
+                alert("Ups. Nos hay esa cantidad disponible.")
+                mostrarProductos()}
+        }
+        else if (dije2 === true){
+                    if (prod5.disponible === true && prod5.stock >= unidades){
+                    alert("Debes abonar $" + unidades * prod5.precio)
+                } 
+                else { 
+                alert("Ups. No hay esa cantidad disponible.")
+                mostrarProductos()}
+        }
+        else if (dije3 === true){
+    
+                if (prod6.disponible === true && prod6.stock >= unidades){
+                    alert("Debes abonar" + unidades * prod6.precio)            
+                } 
+                else { 
+                alert("Ups. No hay esa cantidad disponible.")
+                mostrarProductos()}
+    
+        }     
+        else {
+            console.log(Error);
+        }
+        }
+    
+
+
+
+const mostrarProductos = () => {
+    let lista = ""
+    catalogodijes.forEach(element=>{
+        lista += `${element.id}) ${element.nombre}\n`;}
+        )
+    console.log(lista)
+
+    prodElegido = prompt ("Ingrese el n° de producto\n" + lista);
+        
+    while (prodElegido ==="") {
+        prodElegido = prompt ("Ingrese el n° de producto\n" + lista);
+    }
+
+    if (prodElegido === "1"){
+        dije1 = true;
+        elegirColor()
+        vender()
+    }else if (prodElegido === "2"){
+        dije2 = true;
+        elegirColor()
+        vender()
+    }else if (prodElegido === "3"){
+        dije3 = true;
+        elegirColor()
+        vender()
     }
  
-            
-        return color;
-        return unidades;
-        return dijeazul;
-        return dijerosa;
-        return dijenegro;
+    
+    else{
+        alert ("Ingrese un n° válido")
+        mostrarProductos()
     }
+     
+}
 
-elegir()
-console.log(color)
-console.log(unidades);
+    mostrarProductos()
 
-console.log(dijeazul)
-console.log(dijerosa)
-console.log(dijenegro)
-
-
-function vender(){
-    if (dijeazul === true){
-            if (producto4.disponible === true && producto4.stock >= unidades){
-                alert("Debes abonar $" + unidades * producto4.precio)
-            } 
-            else { 
-            alert("Ups. Nos hay esa cantidad disponible.")}
-    }
-    else if (dijerosa === true){
-                if (producto5.disponible === true && producto5.stock >= unidades){
-                alert("Debes abonar $" + unidades * producto5.precio)
-            } 
-            else { 
-            alert("Ups. No hay esa cantidad disponible.")}
-    }
-    else if (dijenegro === true){
-
-            if (producto6.disponible === true && producto6.stock >= unidades){
-                alert("Debes abonar $" + unidades * producto6.precio)
-            } 
-            else { 
-            alert("Ups. No hay esa cantidad disponible.")}
-
-    }     
-    else {
-        console.log(Error);
-    }
-    }
-          
-vender()
