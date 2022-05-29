ccc2f9610082d64b56af909a55146cacc7865e4b
Marurodriguez <rodriguezsmarisol@gmail.com>
2022-05-28 22:57:22 -0300

    Update main.js

--- tienda/aros/js/main.js
+++ tienda/aros/js/main.js
@@ -1,77 +1,88 @@
-cclass articulo {
-    constructor(id, nombre, precio, stock, disponible){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.disponible = disponible;
-        this.img=img;
-    }
-} 
 
 const catalogo = [
-    {id:1, img: "", nombre:"ARO1", precio:2500, stock: 5, disponible: true},
-    {id:2, img: "", nombre:"ARO2", precio:3000, stock: 5, disponible: true},
-    {id:3, img: "", nombre:"ARO3", precio:2000, stock: 5, disponible: false},
-    {id:4, img: "", nombre:"DIJE1", precio:2000, stock: 5, disponible: true},
-    {id:5, img: "", nombre:"DIJE2", precio:1800, stock: 5, disponible: true},
-
+    {id:1, img: "aro1.png", nombre:"Aro solitario turquesa", precio:2500, stock: 5, disponible: true},
+    {id:2, img:"aro2.png", nombre:"Aro solitario naranja", precio:3000, stock: 5, disponible: true},
+    {id:3, img:"aro3.png", nombre:"Aro solitario violeta", precio:2000, stock: 5, disponible: false},
 ]
 
 console.log(catalogo)
 
-const mostrarProductos = () =>{
+
+const caja = document.getElementById("caja");
+const divCarrito = document.getElementById("divCarrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
+function mostrarCatalogo (){
+    catalogo.forEach(element=> {
+        caja.innerHTML += 
+        `<div class="aro">
+        <h3>${element.nombre}</h3> 
+        <img style="width:250px;" class="imgcatalogo" src="${element.img}">
+        <b>Precio: $${element.precio}</b>
+        <button id="btn${element.id}"> Agregar al carrito </button>
+        </div>
+       `
+    }) 
 
 
-for (const articulo of catalogo){
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = 
-    `<h3>Código: ${articulo.id}</h3> 
-    <img>${articulo.img}</img>
-    <p>${articulo.nombre}</p>
-    <b>Precio: $${articulo.precio}</b>`
-    
-    document.body.appendChild(contenedor);
-    contenedor.className="listado"
-
-}
-}
-mostrarProductos()
-
-const inputCodigo = document.querySelector("#codigo")
-const inputCantidad = document.querySelector("#cantidad")
-
-class prodElegido {
-    constructor (codigo, cantidad){
-    this.codigo = codigo;
-    this.cantidad = cantidad;
-}
-}
-
-const crearCarrito = (codigo, cantidad) => {
-    codigo = inputCodigo.value;
-    cantidad = inputCantidad.value;
-    new prodElegido(codigo, cantidad)
- }
-
-crearCarrito()
-console.log(prodElegido);
-
-const mostrarCarrito = () => {
-        let carrito = document.createElement("p");
-        carrito.innerHTML = 
-        `Debes abonar ${articulo.precio}*${prodElegido.cantidad}`
+    catalogo.forEach(articulo=>{
+        document.querySelector(`#btn${articulo.id}`).addEventListener("click",()=>{
+            console.log(articulo)
+            agregarAlCarrito(articulo)
+           
+            
+                    
+        });
         
-        document.body.appendChild(carrito);
     
+    });
+
+    function agregarAlCarrito(articulo){
+        
+        let existe = carrito.some((element=>element.id === articulo.id))
+        
+        if (!existe){
+            articulo.cantidad = 1;
+            carrito.push(articulo)
+        }else{
+            articulo.cantidad++
+            console.log(carrito)
+        }
+        mostrarCarrito();
+    }
+
+function mostrarCarrito(){
+    divCarrito.innerHTML="";
+    carrito.forEach((element)=>{
+        divCarrito.innerHTML += 
+        `<div class="aro">
+        <h3>${element.nombre}</h3> 
+        <img style="width:100px;" class="imgcatalogo" src="${element.img}">
+        <b>CANTIDAD: ${element.cantidad}</b>
+        <button id="quitar${element.id}"> Quitar del carrito </button>
+        </div>
+       `;
+    });
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    quitarArticulo();
+}
+
+function quitarArticulo(){
+    carrito.forEach((articulo)=>{ 
+        document
+            .querySelector(`#quitar${articulo.id}`)
+            .addEventListener("click",()=>{
+            carrito = carrito.filter(element=>element.id !== articulo.id);
+            mostrarCarrito();
+        });
+    })
+}
 
 }
 
-boton.addEventListener("click", () => {
-    const cargarCarrito = crearCarrito(codigo,cantidad);
-    mostrarCarrito (cargarCarrito)
-
-})
-
+mostrarCatalogo() 
+mostrarCarrito()    
+          
 
 
