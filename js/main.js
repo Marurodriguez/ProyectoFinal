afdcd2a1dfc60bb146b3891d098212262b93fb9b
Marisol Rodríguez <rodriguezsmarisol@gmail.com>
2022-06-25 21:05:52 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
-/* class articulo {
-    constructor(id, nombre, precio, stock, disponible){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.disponible = disponible;
-        this.img=img;
-    }
-}   */
+
 
 const catalogo = [
-    {id:1, img1: "js/img/aro1.png", img2: "js/img/aro1.png", nombre:"Aro solitario turquesa", nombrePlural: "Aros solitarios turquesa", precio:2500, precioDesc: 2250, stock: 5, disponible: true},
-    {id:2, img1:"js/img/aro2.png", img2: "js/img/aro2.png", nombre:"Aro solitario naranja", nombrePlural: "Aros solitarios naranja", precio:3000, precioDesc: 2700, stock: 5, disponible: true},
-    {id:3, img1:"js/img/aro3.png", img2: "js/img/aro3.png", nombre:"Aro solitario violeta", nombrePlural: "Aros solitarios violeta", precio:2000, precioDesc: 1800, stock: 5, disponible: false},
+    {id:1, img1: "js/img/aro1.png", img2: "js/img/aro1slide.jpeg", nombre:"Aro solitario turquesa", nombrePlural: "Aros solitarios turquesa", precio:2500, precioDesc: 2250, stock: 5, disponible: true},
+    {id:2, img1:"js/img/aro2.png", img2: "js/img/aro2slide.jpeg", nombre:"Aro solitario naranja", nombrePlural: "Aros solitarios naranja", precio:3000, precioDesc: 2700, stock: 5, disponible: true},
+    {id:3, img1:"js/img/aro3.png", img2: "js/img/aro3slide.jpeg", nombre:"Aro solitario violeta", nombrePlural: "Aros solitarios violeta", precio:2000, precioDesc: 1800, stock: 5, disponible: false},
 ]
 
 console.log(catalogo)
@@ -23,9 +14,10 @@ const [articulo1, articulo2, articulo3] = catalogo
 const caja = document.getElementById("caja");
 const divCarrito = document.getElementById("divCarrito");
 const listaCompra = document.getElementById("listaCompra");
+const divTotal = document.getElementById("divTotal");
 
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-let compra = JSON.parse(localStorage.getItem("compra")) || [];
+let carrito = []  ||  JSON.parse(localStorage.getItem("carrito"));
+let compra = [] || JSON.parse(localStorage.getItem("compra"));
 
 
 function mostrarCatalogo (){
@@ -38,12 +30,15 @@ function mostrarCatalogo (){
         <h3>${element.nombre}</h3> 
   
         <div class="fade">
-        <div><img style="width:200px;" class="imgcatalogo" src="${element.img1}"></div>
+
+        <div><img style="width:200px; repeat: no" class="imgcatalogo" src="${element.img1}"></div>
         <div><img style="width:200px;" class="imgcatalogo" src="${element.img2}"></div>
+
         </div>
 
-        <b>Precio: <del> $${element.precio} </del>   $${element.precioDesc}</b>
+        <b><u>Precio</u>: $${element.precio}</b>
         <button id="btn${element.id}"> Agregar al carrito </button>
+
         </div>
        `
        
@@ -54,8 +49,10 @@ function mostrarCatalogo (){
     catalogo.forEach(articulo=>{
         document.querySelector(`#btn${articulo.id}`).addEventListener("click",()=>{
             console.log(articulo)
+
             agregarAlCarrito(articulo)
-           
+            mostrarModal()
+    
             
                     
         });
@@ -65,17 +62,30 @@ function mostrarCatalogo (){
 
 function agregarAlCarrito(articulo){
         
-        let existe = carrito.some((element=>element.id === articulo.id))
+        let existe = carrito.some((element=> element.id === articulo.id))
         
         if (!existe){
             articulo.cantidad = 1,
             carrito.push(articulo)
+
         }else{
             articulo.cantidad++
             console.log(carrito)
         }
+
+
         mostrarCarrito();
+        mostrarCompra();
     }
+      
+    const modal = document.getElementById("modal");    
+
+function mostrarModal(){
+    $(document).ready(function() {
+    $("#modal").css({"display" : "block"}).fadeOut(4000)
+    })};
+
+
 
 function mostrarCarrito(){
     divCarrito.innerHTML="";
@@ -88,9 +98,10 @@ function mostrarCarrito(){
         </div>
        `;
     });
+
     localStorage.setItem("carrito", JSON.stringify(carrito));
     quitarArticulo();
-    mostrarCompra();
+   
 }
 
 function quitarArticulo(){
@@ -98,8 +109,17 @@ function quitarArticulo(){
         document
             .querySelector(`#quitar${articulo.id}`)
             .addEventListener("click",()=>{
+                let existe = carrito.some((element=> element.id === articulo.id))
+        
+        if (articulo.cantidad > 1){
+            articulo.cantidad--
+
+        }else{
+            
             carrito = carrito.filter(element=>element.id !== articulo.id);
             compra = compra.filter(element=>element.id !== articulo.id);
+        }
+
             mostrarCarrito();
             mostrarCompra();
         });
@@ -112,42 +132,48 @@ function quitarArticulo(){
 
 function mostrarCompra(){
     listaCompra.innerHTML="";
-    carrito.forEach((element)=>{
+    carrito.forEach((element)=>{  
         element.cantidad === 1 
         ? 
         listaCompra.innerHTML +=
         `<li>
-        ${element.cantidad} ${element.nombre}: $${element.precioDesc}</b> 
+        ${element.cantidad} ${element.nombre}: $${element.precio}</b> 
         </li>
        `
        :
         listaCompra.innerHTML +=
         `<li>
-        ${element.cantidad} ${element.nombrePlural}: $${element.precioDesc* element.cantidad}</b> 
+        ${element.cantidad} ${element.nombrePlural}: $${element.precio* element.cantidad}</b> 
         </li> 
        `
        }
     )
-    };
+    mostrarTotal()
+};
 
 
-    const pagar = document.getElementById("pagar")
 
-pagar.addEventListener("click", () =>{
+function mostrarTotal(){
+    let total = 0;
+    divTotal.innerHTML="";
+    carrito.forEach((element)=>{
 
-    Swal.fire ({
-        title: "Exito!",
-        text: "Gracias por tu compra",
-        icon: "success",
-        confirmButtonText: "Salir"
+        const precio = element.precio
+        const cantidad = element.cantidad
+
+        total = total + precio*cantidad
+
+        console.log(total);
+
+        divTotal.innerHTML =
+        `<h4><u>TOTAL</u>:
+        $${total}
+        </h4>
+       `
     })
-})
-
+ 
+} 
 
 mostrarCatalogo() 
 
-setTimeout(()=>{
 
-Swal.fire("Sólo por hoy, aprovecha el 10% de descuento!")}
-
-, 2000)
